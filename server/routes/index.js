d4134eb34b7b56a56972597059c4fb9d40517a2f
Gaiasys-Trilok <trilok@gaiasys.in>
2019-11-09 12:00:10 +0530

    added server code

---
+++ server/routes/index.js
@@ -0,0 +1,32 @@
+var Category = require("../models/category");
+var Post = require("../models/post");
+router.get('/category', function(req, res, next) {
+    Category.find(function (err, categories) {
+      if (err) return next(err);
+      res.json(categories);
+    });
+  });
+  
+  router.get('/bycategory/:id', function(req, res, next) {
+    Post.find({category: req.params.id}, function (err, posts) {
+      if (err) return next(err);
+      res.json(posts);
+    });
+  });
+  
+  router.get('/post', function(req, res, next) {
+    Post.find(function (err, posts) {
+      if (err) return next(err);
+      res.json(posts);
+    });
+  });
+  
+  router.get('/post/:id', function(req, res, next) {
+    Post.findById(req.params.id, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  });
+  
+  module.exports = router;
+  
