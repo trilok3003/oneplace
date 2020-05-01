a86a42fcf50b14a1ee8d2f9d89eec8265b855aaf
gaiasys <trilok@gaiasys.in>
2020-05-01 21:19:25 +0530

    Meta service in Angular. Add/Update Meta Tags Example

--- projects/seo/src/app/home/home.component.ts
+++ projects/seo/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private metaService:Meta){
   }
+ 
+  ngOnInit() {
+    this.metaService.addTag( { name:'description',content:"Article Description"});
+  }
+  addTag() {
+    this.metaService.addTag({ name: 'description', content: 'Article Description' });
+    this.metaService.addTag({ name: 'robots', content: 'index,follow' });
+    this.metaService.addTag({ property: 'og:title', content: 'Content Title for social media' });
+  }
+ addTags() {
+  this.metaService.addTags([
+    { name: 'description', content: 'Article Description' },
+    { name: 'robots', content: 'index,follow' },
+    { property: 'og:title', content: 'Content Title for social media' }
+ ]);
+}
+duplicateTags() {
+  this.metaService.addTag( { name: 'description', content: 'Article Description' })
+  this.metaService.addTag( { name: 'description', content: 'Article Description' })
+}
+duplicateTags1() {
+  this.metaService.addTag( { name: 'description', content: 'Article Description' })
+  this.metaService.addTag( { name: 'description', content: 'A different Article Description' })
+}
+duplicateTags2() {
+  this.metaService.addTag( { name: 'description', content: 'Article Description' })
+  this.metaService.addTag( { name: 'description', content: 'A different Article Description' })
+  this.metaService.addTag( { name: 'description', content: 'Description of the Article' })   //Not Inserted
+  this.metaService.addTag( { name: 'description', content: 'A different Article Description' })    //Inserted
+}
+
 
 }
